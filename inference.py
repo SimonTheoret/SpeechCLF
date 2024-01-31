6678cb537c7bc5efbba70b37f76f8299b27947fb
SimonTheoret <simonteoret@hotmail.com>
2024-01-31 09:58:41 -0500

    Tests for inference; split function; preprocess_str

--- inference.py
+++ inference.py
@@ -1,26 +1,39 @@
+from typing import Any
+
 import fire
 import lxml.html
 import requests
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+from preprocessing.data_processing import preprocess_str
 
 
 def inference(
     text: str,
-    path: str = "models/distilbert-base-uncased/run_5/checkpoint-2304/",
-    name: str = "distilbert-base-uncased",
-):
+    path: str = "output/google/MobileBert",
+    name: str = "google/mobilebert-uncased",
+) -> Any:
+    """Prints and returns the output of the selected model."""
     model = AutoModelForSequenceClassification.from_pretrained(path).eval()
     tokenizer = AutoTokenizer.from_pretrained(name)
     out = model(**tokenizer([text], return_tensors="pt"))
     print(out)
+    return out
 
 
-def get_html_content(url: str, max_size: int):
+def get_html_content(url: str, max_size: int, length: int) -> list[str]:
     try:
         str_content = download_limited(url, max_size)
     except Exception as e:
-        return e
-    return parse_content(str_content)
+        raise e
+
+    lemmatizer = WordNetLemmatizer()
+    stopwords_english = stopwords.words("english")
+    str_content = preprocess_str(str_content, lemmatizer, stopwords_english, None)
+    str_content = parse_content(str_content)
+    return split_strings(str_content, length)
 
 
 def download_limited(url, max_size) -> str:
@@ -33,25 +46,37 @@ def download_limited(url, max_size) -> str:
     if code > 300 and code < 200:
         raise Exception(f"received status code {code}")
 
-    if int(res.headers.get("Content-Length", 0)) > max_size:
-        raise Exception(f"response size went over {max_size} limit")
+    if int(size := res.headers.get("Content-Length", 0)) > max_size:
+        raise Exception(f"response size (of size {size}) went over {max_size} limit")
 
     data = []
     length = 0
 
     for chunk in res.iter_content(1024):
-        data.append(chunk)
+        data.append(chunk.decode())
         length += len(chunk)
         if length > max_size:
-            raise Exception(f"response size went over {max_size} limit")
-
+            raise Exception(f"response size {length} went over {max_size} bytes limit")
+    print(f"size of response: { length }")
     return "".join(data)
 
 
-def parse_content(text: str):
-    """Parses the response html content and returns only text"""
-    page = lxml.html.document_fromstring(text)
-    page.cssselect("body")[0].text_content()
+def parse_content(text: str) -> str:
+    """Parses the response html content and returns only the text content"""
+    page = lxml.html.fromstring(text)
+    return page.cssselect("body")[0].text_content()
+
+
+def split_strings(text: str, length: int) -> list[str]:
+    "Split string into a list of string of specified length"
+    text_len = len(text)
+    splitted = [text[i : i + length] for i in range(0, text_len, length)]
+    if text_len % length != 0:  # if length does not divide the length of the text
+        remaining = text_len % length  # characters not in the splitted
+        splitted.append(
+            text[-remaining - 1 : -1]
+        )  # include -remaining-1 as it is not included in splitted
+    return splitted
 
 
 if __name__ == "__main__":
--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,10 +1,44 @@
-from inference import get_html_content, download_limited, parse_content
+import pytest
+
+from inference import download_limited, get_html_content, parse_content
+
 
 def test_parse_content():
-    with open("Active_learning.html", r)
+    with open("tests/Active_learning.html", "r") as f:
+        content = f.read()
+        text: str = parse_content(content)
+        assert text is not None  # Verify if content is correctly parsed
+        assert "Active learning" in text
+        assert "Definitions" in text
+        assert "Balance exploration and exploitation" in text
+        assert "Settles, Burr" in text
+        assert "There are situations in which" in text
+
 
 def test_download_limited():
-    pass
+    with pytest.raises(Exception):
+        download_limited(
+            "https://en.wikipedia.org/wiki/Active_learning_(machine_learning)",
+            20,  # 20 bytes
+        )
+    download_limited(
+        "https://en.wikipedia.org/wiki/Active_learning_(machine_learning)",  # ~250KB
+        300_000,  # 300,000 bytes
+    )
+
 
 def test_get_html_content():
-    pass
+    with pytest.raises(Exception):  # will raise an exception
+        get_html_content(
+            "https://en.wikipedia.org/wiki/Active_learning_(machine_learning)", 20
+        )
+
+    text = get_html_content(  # will not raise an exception
+        "https://en.wikipedia.org/wiki/Active_learning_(machine_learning)", 300_000
+    )
+    assert text is not None  # Verify if content is correctly parsed
+    assert "Active learning" in text
+    assert "Definitions" in text
+    assert "Balance exploration and exploitation" in text
+    assert "Settles, Burr" in text
+    assert "There are situations in which" in text
